1aa78c35af31dbddc700816593c646dd5585d499
neurohacked <admin@neurohacked.com>
2016-10-14 01:20:29 -0700

    Use root for home.
    Use root instead of /ramen for home page.

--- controllers/app_controller.js
+++ controllers/app_controller.js
@@ -3,7 +3,22 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', function(req, res) {
-    res.redirect('/ramen');
+    models.Ramen.findAll({
+            include: [models.User]
+        })
+        // connect the findAll to this .then
+        .then(function(data) {
+            // grab the user info from our req.
+            // How is it in our req?
+            // This info gets saved to req via the users_controller.js file.
+            res.render('index', {
+                user_id: req.session.user_id,
+                username: req.session.user_name,
+                email: req.session.user_email,
+                logged_in: req.session.logged_in,
+                ramen: data
+            });
+        });
 });
 
 module.exports = router;
--- controllers/ramen_controller.js
+++ controllers/ramen_controller.js
@@ -2,25 +2,6 @@ const models = require('../models');
 const express = require('express');
 const router = express.Router();
 
-router.get('/', function(req, res) {
-    models.Ramen.findAll({
-            include: [models.User]
-        })
-        // connect the findAll to this .then
-        .then(function(data) {
-            // grab the user info from our req.
-            // How is it in our req?
-            // This info gets saved to req via the users_controller.js file.
-            res.render('ramen/index', {
-                user_id: req.session.user_id,
-                username: req.session.user_name,
-                email: req.session.user_email,
-                logged_in: req.session.logged_in,
-                ramen: data
-            });
-        });
-});
-
 router.post('/create', function(req, res) {
     models.Ramen.create({
             ramen_name: req.body.ramen_name,
